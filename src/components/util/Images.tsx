f9eb7d3a9ca136ade10874ff860e5ce3db42d671
LeoRaab <leopold.raab@gmail.com>
2022-08-19 14:01:34 +0200

    added image-club componenet

--- src/components/util/Images.tsx
+++ src/components/util/Images.tsx
@@ -2,3 +2,6 @@ import * as React from "react";
 import Logo from "../../assets/images/logo.png";
 
 export const ImageLogo = (props: { style?: React.CSSProperties }) => <img alt="Logo" src={Logo} style={props.style} />;
+export const ImageClub = (props: { clubName: string; imageSrc: string; style?: React.CSSProperties }) => (
+    <img alt={props.clubName} src={props.imageSrc} style={props.style} />
+);
