275784208fcd685a3225ae3c4ca718773ecf2905
LeoRaab <leopold.raab@gmail.com>
2022-08-19 14:02:45 +0200

    changed prefix of root route

--- src/components/clubs/routes/ClubsRoutes.ts
+++ src/components/clubs/routes/ClubsRoutes.ts
@@ -3,6 +3,6 @@ import { Routes } from "../../app/router/Routes";
 const prefix = (route: string) => Routes.ROOT + route;
 
 export const ClubsRoutes = {
-    ROOT: prefix("/"),
+    ROOT: prefix(""),
     DETAIL: prefix("detailsview/:clubId"),
 };
