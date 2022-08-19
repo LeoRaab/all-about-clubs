9fc92b7c750293f20ca97f8df892f7ce2c38c4a6
LeoRaab <leopold.raab@gmail.com>
2022-08-19 14:21:53 +0200

    removed not used routes

--- src/components/app/router/AppRouter.tsx
+++ src/components/app/router/AppRouter.tsx
@@ -3,12 +3,8 @@ import * as React from "react";
 import { Router } from "react-router-dom";
 import { generalStore } from "../../../stores/GeneralStore";
 import { LoadingOverlay } from "../../ui/LoadingOverlay";
-import { AuthLoginSite } from "../../auth/sites/AuthLoginSite";
 import { CustomSwitch } from "./CustomSwitch";
 import { history } from "./history";
-import { NoAuthOnlyRoute } from "./NoAuthOnlyRoute";
-import { PrivateRoute } from "./PrivateRoute";
-import { Routes } from "./Routes";
 import { RoutingManager } from "./RoutingManager";
 import { ClubsContainerSite } from "../../clubs/sites/ClubsContainerSite";
 
@@ -18,12 +14,6 @@ export const AppRouter = observer(() => (
             <RoutingManager>
                 <CustomSwitch>
                     <ClubsContainerSite />
-                    {/* <NoAuthOnlyRoute exact path={Routes.ROOT}>
-                        <AuthLoginSite />
-                    </NoAuthOnlyRoute>
-                    <PrivateRoute path={DashboardRoutes.ROOT}>
-                        <DashboardContainerSite />
-                    </PrivateRoute> */}
                 </CustomSwitch>
             </RoutingManager>
         </Router>
